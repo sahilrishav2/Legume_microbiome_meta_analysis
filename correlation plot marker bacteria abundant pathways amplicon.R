e0380b8d313eee581a263802fc3ebc49238f8274
Rishav Sahil <153201461+sahilrishav2@users.noreply.github.com>
2024-01-30 22:15:50 +0530

    Update correlation plot marker bacteria abundant pathways amplicon.R

--- correlation plot marker bacteria abundant pathways amplicon.R
+++ correlation plot marker bacteria abundant pathways amplicon.R
@@ -3,30 +3,25 @@
   
 library(corrplot)
 library(psych)
-p <- read.csv("path3.csv",row.names=1)
-s <- read.csv("species3.csv",row.names=1)
-metrix_p <-corr.test(p,s,method="spearman",adjust="fdr")
-matrix_q <- data.frame(metrix_p["p.adj"])
-matrix_rel_eth <- cor(p,s,method="spearman",use="complete.obs")
-colnames(matrix_q) <- colnames(matrix_rel_eth)
-matrix_q <- as.matrix(matrix_q)
-str(matrix_rel_eth)
-str(matrix_q)
-png("corr_mat2.png",height=5400,width=9800,res=600)
-corrplot(matrix_rel_eth,
-         #title = "Correlation Plot",
+p <- read.csv("path.csv",row.names=1)
+s <- read.csv("species.csv",row.names=1)
+m1 <-corr.test(p,s,method="spearman",adjust="fdr")
+m2 <- data.frame(m1["p.adj"])
+matrix_corr <- cor(p,s,method="spearman",use="complete.obs")
+colnames(m2) <- colnames(matrix_rel_eth)
+m2 <- as.matrix(m2)
+str(matrix_corr)
+str(m2)
+png("corrplot.png",height=5400,width=9800,res=600)
+corrplot(matrix_corr,
          method = "circle",
          outline = T, 
          addgrid.col = "grey", 
          p.mat= matrix_q,
          sig.level = 0.01,
-         insig ="blank",
-         #mar = c(0,0,1,0), 
+         insig ="blank", 
          addrect = 4, 
-         #rect.col = "grey", 
-         #rect.lwd = 1,
          cl.pos = "b", 
-         #        tl.pos = "ld",
          tl.col = "black", #colour of text labels 
          tl.cex = 0.5, #Size of text labels
          cl.cex = 0.5)
